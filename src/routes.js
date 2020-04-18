6c4f48c5d3870cd8d14f01d7b4ded13b1fd3d280
alinareabteva <52407474+alinareabteva@users.noreply.github.com>
2020-04-18 16:43:23 +0300

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// Импорт самого роутера
+// Switch для переключения по роутам
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Welcome from './components/Welcome/Welcome';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+
+const Routes = () => (
+<BrowserRouter>
+<Switch>
+<Route exact path='/' component={Welcome} />
+<Route  path='/login' component={Login} />
+<Route  path='/signup' component={Signup} />
+<Route  path='/home' component={Home} />
+<Route  path='/*' component={NotFound} />
+
+</Switch>
+</BrowserRouter>
+);
+export default Routes;
